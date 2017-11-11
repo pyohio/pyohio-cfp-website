eb7bcfd0548c0d27e4b7af73cb30d82a92890798
Joshua Simmons <i@joshuasimmons.name>
2017-11-11 15:18:55 -0800

    add accessibility and accommodation guide

--- pinaxcon/urls.py
+++ pinaxcon/urls.py
@@ -39,6 +39,8 @@ urlpatterns = [
     url(r"^attend/travel$", TemplateView.as_view(template_name="static_pages/attend/travel.html"), name="attend/travel"),
     url(r"^attend/hotels$", TemplateView.as_view(template_name="static_pages/attend/hotels.html"), name="attend/hotels"),
     url(r"^attend/tshirt$", TemplateView.as_view(template_name="static_pages/attend/tshirt.html"), name="attend/tshirt"),
+    url(r"^attend/accessibility-and-accommodations$",TemplateView.as_view(template_name="static_pages/attend/accommodations.html"), name="attend/accessibility-and-accommodations"),
+    url(r"^accessibility$", RedirectView.as_view(url="attend/accessibility-and-accommodations")),
 
     url(r"^code-of-conduct$", TemplateView.as_view(template_name="static_pages/code_of_conduct/code_of_conduct.html"), name="code-of-conduct"),
     url(r"^code-of-conduct/harassment-incidents$", TemplateView.as_view(template_name="static_pages/code_of_conduct/harassment_procedure_attendee.html"), name="code-of-conduct/harassment-incidents"),
