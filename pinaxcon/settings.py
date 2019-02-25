5012c8d32b9026c51790143ccef8b48bb2807546
Dave Forgac <tylerdave@tylerdave.com>
2019-02-25 00:35:28 -0500

    chance cache backends

--- pinaxcon/settings.py
+++ pinaxcon/settings.py
@@ -17,11 +17,11 @@ if REDIS_URL:
     redis_url = urlparse.urlparse(REDIS_URL)
     CACHES = {
         "default": {
-            "BACKEND": "redis_cache.RedisCache",
-            "LOCATION": "{0}:{1}".format(redis_url.hostname, redis_url.port),
+            "BACKEND": "django_redis.cache.RedisCache",
+            "LOCATION": "{0}:{1}/0".format(redis_url.hostname, redis_url.port),
             "OPTIONS": {
+                "CLIENT_CLASS": "django_redis.client.DefaultClient",
                 "PASSWORD": redis_url.password,
-                "DB": 0,
             }
         }
     }
